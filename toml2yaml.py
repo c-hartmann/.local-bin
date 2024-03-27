e3ab0a590b6a16bf97351b1facba2bd65d45da07
Christian Hartmann <hartmann.christian@gmail.com>
2024-03-27 12:40:02 +0100

    replace link with real file

--- toml2yaml.py
+++ toml2yaml.py
@@ -1,1 +1,13 @@
-/home/christian/.local/bin/t2y.py
+#!/usr/bin/python3
+
+import sys
+
+if len(sys.argv) < 2:
+	print("error: file argument required")
+	sys.exit(1)
+
+import toml
+import yaml
+
+dict=toml.load(open(sys.argv[1],"r"))
+print(yaml.dump(dict))
